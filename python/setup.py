7da502aba6b2d3c299565ada92d53501d68b9002
Hussain Chinoy <ghchinoy@google.com>
2026-08-23 17:35:40 +0000

    Configure cibuildwheel and dry-run release workflow for v0.1.0
    - Add workflow_dispatch trigger with pre-release and PyPI publish controls
    - Configure cibuildwheel for macOS arm64 and Linux x86_64 manylinux wheels
    - Add python/setup.py with BinaryDistribution to ensure platform-tagged wheels
    - Guard PyPI publishing against pre-releases and manual dry-runs

---
+++ python/setup.py
@@ -0,0 +1,24 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+from setuptools.dist import Distribution
+
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
+
+
+setup(distclass=BinaryDistribution)
